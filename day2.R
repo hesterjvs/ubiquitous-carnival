f57115c1fb1417510ac1745afdbaa7e0c100f677
hesterjvs <hestervs@gmail.com>
2024-01-25 12:22:23 +0000

    Graph of data

--- day2.R
+++ day2.R
@@ -13,3 +13,11 @@ go <- read.csv("data/GardenOzone.csv")
 head(go)
 glimpse(go)
 str(go)
+
+#plot data
+p1 <- ggplot(go, aes(x= Ozone)) +
+  geom_histogram(bins = 9) +
+  facet_wrap(~Garden.location, ncol = 1) +
+  expand_limits(x = 40) +
+  scale_x_continuous(breaks = c(40,60,80))
+p1
